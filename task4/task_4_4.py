0aeaec88abb5edd15c355ab112fdcb6f165d5623
ViktorVersh <vershininzaig@mail.ru>
2025-01-08 21:11:24 +0800

    решал доп. задачи

---
+++ task4/task_4_4.py
@@ -0,0 +1,35 @@
+"""
+    Копирование файлов:
+    - Реализуйте функцию для копирования содержимого одного текстового файла в другой. Убедитесь, что если
+    целевой файл уже существует, программа предупреждает пользователя и предлагает перезаписать или отменить операцию.
+"""
+import os
+
+
+def copy_file(source, target):
+    """
+    Функция копирования содержимого одного текстового файла в другой.
+    :return:
+    """
+    try:
+        with open(source, 'r',encoding='utf-8') as source_file:
+            if os.path.exists(target):
+                print('Файл уже существует. Перезаписать?')
+                answer = input('y/n: ')
+                if answer == 'y':
+                    with open(target, 'w', encoding='utf-8') as target_file:
+                        for line in source_file:
+                            target_file.write(line)
+                else:
+                    print('Копирование отменено')
+            else:
+                with open(target, 'w', encoding='utf-8') as target_file:
+                    for line in source_file:
+                        target_file.write(line)
+    except FileNotFoundError:
+        print("Файл не найден")
+    except IOError:
+        print('Ошибка чтения файла')
+
+
+copy_file('test.txt', "test_1.txt")
